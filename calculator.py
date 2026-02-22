8e32e9682151547fd21866e457001582f062e33c
David <davidnwachukwunf1@gmail.com>
2026-02-22 20:56:14 +0100

    A basic python calcuulator

---
+++ calculator.py
@@ -0,0 +1,39 @@
+while True:
+
+ print('Select operation:')
+ print('1. Add')
+ print('2. Subtract')
+ print('3. Multiply')
+ print('4. Divide')
+ print('5. Exponentiate')
+ print('6. Exit')
+
+ choice = input('Enter choice (1/2/3/4/5/6): ')
+
+ if choice == '6':
+    break
+ if choice not in ['1', '2', '3', '4', '5']:
+    print('invalid input, try again!')
+    break 
+
+ num1 = (input('Enter first number: '))
+ num2 = (input('Enter second number: '))
+
+ if choice == '1':
+    print('Result:', int(num1) + int(num2))
+ elif choice == '2':
+    print('Result:', int(num1) - int(num2))
+ elif choice == '3':
+    print('Result:', int(num1) * int(num2))
+ elif choice == '4':
+    if num2 != 0:
+       print('Result:', int(num1) / int(num2))
+    else:
+        print('Error: Division by zero')
+ elif choice == '5':
+    if num1 != 0 and num2 != 0:
+       print('Result:', int(num1) ** int(num2))
+    else:
+        print('Error: Exponentiation with zero')
+ else:
+    print('Invalid input, try again!')
