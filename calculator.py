64b25f098d75c16a7374f0f5462c8f50c7a2bd54
David <davidnwachukwunf1@gmail.com>
2026-03-16 20:03:49 +0100

    Added 1 operation, find the root

--- calculator.py
+++ calculator.py
@@ -1,24 +1,31 @@
 while True:
-
+ 
+      # Display menu with operations #
  print('Select operation:')
  print('1. Add')
  print('2. Subtract')
  print('3. Multiply')
  print('4. Divide')
  print('5. Exponentiate')
- print('6. Exit')
+ print('6. Root')
+ print('7. Exit')
+      
+      # Choose operations from menu #
+ choice = input('Enter choice (1/2/3/4/5/6/7): ')
 
- choice = input('Enter choice (1/2/3/4/5/6): ')
-
- if choice == '6':
+      # ensure accurate operation selection #
+ if choice == '7':
+    print('Exiting...')
     break
- if choice not in ['1', '2', '3', '4', '5']:
+ if choice not in ['1', '2', '3', '4', '5', '6', '7']:
     print('invalid input, try again!')
     break 
 
- num1 = (input('Enter first number: '))
- num2 = (input('Enter second number: '))
+      # Recieve Input data which must be intergers #
+ num1 = int(input('Enter first number: '))
+ num2 = int(input('Enter second number: '))
 
+      # Starting operations based on user earlier selection #
  if choice == '1':
     print('Result:', int(num1) + int(num2))
  elif choice == '2':
@@ -29,11 +36,19 @@ while True:
     if num2 != 0:
        print('Result:', int(num1) / int(num2))
     else:
-        print('Error: Division by zero')
+        print('Error: Divided by zero')
  elif choice == '5':
     if num1 != 0 and num2 != 0:
        print('Result:', int(num1) ** int(num2))
     else:
         print('Error: Exponentiation with zero')
+ elif choice == '6':
+    if num1 != 0 and num2 != 0:
+        print('Result:', int(num1) * (int(num2) ** 0.5))
+    else:
+        print('Error: It is zero or negative value')
+ elif choice == '7':
+    print('Exiting...')
+    break
  else:
     print('Invalid input, try again!')
