593d7c697977fdcf3c63148ca176205c8ec9ba4a
David <davidnwachukwunf1@gmail.com>
2026-03-17 21:41:46 +0100

    added percentage and hypotenus calculation

--- calculator.py
+++ calculator.py
@@ -2,22 +2,24 @@ while True:
  
       # Display menu with operations #
  print('Select operation:')
- print('1. Add')
+ print('1. Add', end='     ')
  print('2. Subtract')
- print('3. Multiply')
+ print('3. Multiply', end='     ')
  print('4. Divide')
- print('5. Exponentiate')
+ print('5. Exponentiate', end='     ')
  print('6. Root')
- print('7. Exit')
+ print('7. Percentage', end='   ')
+ print('8. Hypotenuse')
+ print('9. Exit')
       
       # Choose operations from menu #
- choice = input('Enter choice (1/2/3/4/5/6/7): ')
+ choice = input('Enter choice (1/2/3/4/5/6/7/8/9): ')
 
       # ensure accurate operation selection #
- if choice == '7':
+ if choice == '9':
     print('Exiting...')
     break
- if choice not in ['1', '2', '3', '4', '5', '6', '7']:
+ if choice not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
     print('invalid input, try again!')
     break 
 
@@ -48,6 +50,13 @@ while True:
     else:
         print('Error: It is zero or negative value')
  elif choice == '7':
+    if num2 != 0:
+        print('Result:', int((int(num1) / int(num2)) * 100), '%')
+    else:
+        print('Error: Divided by zero')
+ elif choice == '8':
+    print('Result:', float('{:.2f}'.format((int(num1) ** 2 + int(num2) ** 2) ** 0.5)))
+ elif choice == '9':
     print('Exiting...')
     break
  else:
